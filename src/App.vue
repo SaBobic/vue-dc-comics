9516ed3cecac6fb6d5a7bced26917d81c26bedc3
SaBobic <fabrizioettori@gmail.com>
2022-07-25 16:18:16 +0200

    centralized footer menu

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
-    <MainHeader :mainMenuItems="mainMenuItems" />
+    <MainHeader :main-menu-items="mainMenuItems" />
     <main>
       <JumbSection />
       <SeriesSection :series="series" />
       <TabsSection />
     </main>
-    <MainFooter />
+    <footer>
+      <MainFooterTop :footer-menu-items="footerMenuItems" />
+      <MainFooterBottom />
+    </footer>
   </div>
 </template>
 
@@ -15,7 +18,8 @@ import MainHeader from './components/MainHeader.vue';
 import TabsSection from './components/TabsSection.vue';
 import JumbSection from './components/JumbSection.vue';
 import SeriesSection from './components/SeriesSection.vue';
-import MainFooter from './components/MainFooter.vue';
+import MainFooterBottom from './components/MainFooterBottom.vue'
+import MainFooterTop from './components/MainFooterTop.vue'
 
 export default {
   name: 'App',
@@ -147,6 +151,128 @@ export default {
           current: false,
         },
       ],
+      footerMenuItems: [
+        {
+          title: 'DC COMICS',
+          items: [
+            {
+              anchor: 'Characters',
+              url: '#',
+            },
+            {
+              anchor: 'Comics',
+              url: '#',
+            },
+            {
+              anchor: 'Movies',
+              url: '#',
+            },
+            {
+              anchor: 'TV',
+              url: '#',
+            },
+            {
+              anchor: 'Games',
+              url: '#',
+            },
+            {
+              anchor: 'Videos',
+              url: '#',
+            },
+            {
+              anchor: 'News',
+              url: '#',
+            },
+          ]
+        },
+        {
+          title: 'SHOP',
+          items: [
+            {
+              anchor: 'Shop DC',
+              url: '#',
+            },
+            {
+              anchor: 'Shop DC Collectibles',
+              url: '#',
+            },
+          ]
+        },
+        {
+          title: 'DC',
+          items: [
+            {
+              anchor: 'Terms Of Use',
+              url: '#',
+            },
+            {
+              anchor: 'Privacy Policy',
+              url: '#',
+            },
+            {
+              anchor: 'Ad Choices',
+              url: '#',
+            },
+            {
+              anchor: 'Advertising',
+              url: '#',
+            },
+            {
+              anchor: 'Jobs',
+              url: '#',
+            },
+            {
+              anchor: 'Subscriptions',
+              url: '#',
+            },
+            {
+              anchor: 'Talent Workshops',
+              url: '#',
+            },
+            {
+              anchor: 'CPSC Certificates',
+              url: '#',
+            },
+            {
+              anchor: 'Ratings',
+              url: '#',
+            },
+            {
+              anchor: 'Shop Help',
+              url: '#',
+            },
+            {
+              anchor: 'Contact Us',
+              url: '#',
+            },
+          ]
+        },
+        {
+          title: 'SITES',
+          items: [
+            {
+              anchor: 'DC',
+              url: '#',
+            },
+            {
+              anchor: 'MAD Magazine',
+              url: '#',
+            },
+            {
+              anchor: 'DC Kids',
+              url: '#',
+            },
+            {
+              anchor: 'DC Universe',
+              url: '#',
+            },
+            {
+              anchor: 'DC Power Visa',
+              url: '#',
+            },
+          ]
+        },
+      ]
     };
   },
   computed: {
@@ -159,7 +285,8 @@ export default {
     TabsSection,
     JumbSection,
     SeriesSection,
-    MainFooter,
+    MainFooterBottom,
+    MainFooterTop,
   },
 }
 </script>
